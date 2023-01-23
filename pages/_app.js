a190ba6fb876b82dd7c17ac5de3c5cd33bfa77a9
Subash Shumsher Gautam <51912776+shumshersubashgautam@users.noreply.github.com>
2023-01-23 23:12:31 +0545

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import '../styles/globals.css'
+
+const WalletConnectionProvider = dynamic(() => import('../context/WalletConnectionProvider'), {
+    ssr: false,
+})
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <title>Shumsher Solana Cash App</title>
+            </Head>
+            
+            <WalletConnectionProvider>
+                <Component {...pageProps} />
+            </WalletConnectionProvider>
+        </>
+    )
+}
+
+export default MyApp
